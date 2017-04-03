f0f22632caf2b412c2fadbaa24fe69f303f3b4ca
Ricovl <rico@nlsite.com>
2017-04-03 21:47:29 +0200

    Fixed drawing kong and some problems with the end of stage cinematics.

--- src/kong.c
+++ src/kong.c
@@ -21,44 +21,6 @@
 #include "images.h"
 
 
-kong_t kong;
-
-/* Draw kong to the screen and the buffer(does not work if y position or height has changed) */
-void draw_kong(void) {
-	uint8_t width_old = kong.background_data[0];
-	uint8_t width;
-	uint24_t blitx, x = kong.x;
-
-	if (kong.sprite) {
-		if		(kong.sprite == 1)	x -= 4;
-		else if (kong.sprite == 3)	x++;
-		else if (kong.sprite == 4)	x -= 4;
-		else if (kong.sprite == 5)	x -= 2;
-	}
-
-	// Clear kong in buffer and store background
-	gfx_Sprite_NoClip((gfx_image_t*)kong.background_data, kong.x_old, kong.y_old);
-	kong.background_data[0] = *(uint8_t*)kong_sprite[kong.sprite];
-	gfx_GetSprite((gfx_image_t*)kong.background_data, x, kong.y);
-
-	// Draw kong
-	gfx_TransparentSprite_NoClip(kong_sprite[kong.sprite], x, kong.y);
-
-	// Copy the sprite to the buffer and the erased area ^
-	if (kong.x_old < x) {
-		blitx = kong.x_old;
-		width = (x + kong.background_data[0]) - blitx;
-	}
-	else {
-		blitx = x;
-		width = (kong.x_old + width_old) - blitx;
-	}
-	gfx_BlitRectangle(gfx_buffer, blitx, kong.y, width, 32);
-
-	kong.x_old = x;
-	kong.y_old = kong.y;
-}
-
 void update_kong(void) {
 	if (game.stage == STAGE_CONVEYORS) {
 		kong.x += conveyorVector_top;
@@ -113,9 +75,51 @@ update_pauline:
 	draw_pauline(true);
 }
 
+kong_t kong;
+
+/* Draw kong to the screen and the buffer(does not work if y position or height has changed) */
+void draw_kong(void) {
+	uint8_t width_old = kong.background_data[0];
+	uint8_t width;
+	uint24_t blitx, x = kong.x;
+
+	if (kong.sprite) {
+		if (kong.sprite == 1)	x -= 4;
+		else if (kong.sprite == 3)	x++;
+		else if (kong.sprite == 4)	x -= 4;
+		else if (kong.sprite == 5)	x -= 2;
+	}
+
+	// Clear kong in buffer and store background
+	gfx_Sprite_NoClip((gfx_image_t*)kong.background_data, kong.x_old, kong.y_old);
+	kong.background_data[0] = *(uint8_t*)kong_sprite[kong.sprite];
+	gfx_GetSprite((gfx_image_t*)kong.background_data, x, kong.y);
+
+	// Draw kong
+	gfx_TransparentSprite_NoClip(kong_sprite[kong.sprite], x, kong.y);
+
+	// Copy the sprite to the buffer and the erased area ^
+	if (kong.x_old < x) {
+		blitx = kong.x_old;
+		width = (x + kong.background_data[0]) - blitx;
+		if (blitx + width < blitx + width_old)
+			width += (blitx + width_old) - (blitx + width);
+	}
+	else {
+		blitx = x;
+		width = (kong.x_old + width_old) - blitx;
+		if (blitx + width < x + kong.background_data[0])
+			width = kong.background_data[0];
+	}
+	gfx_BlitRectangle(gfx_buffer, blitx, kong.y, width, 32);
+
+	kong.x_old = x;
+	kong.y_old = kong.y;
+}
 
 pauline_t pauline;
 
+/* Draw pauline to the screen and buffer with help or without */
 void draw_pauline(bool help) {
 	uint24_t x = pauline.x;
 
@@ -136,19 +140,23 @@ void draw_pauline(bool help) {
 }
 
 
+
+
 gfx_image_t *kong_knockedout_spite[2] = { knockedout_sprite1, knockedout_sprite0 };
 gfx_image_t *kong_eye[2] = { kong_eye2, kong_eye1 };
 
 void waitTicks(uint8_t ticks) {
+	timer_1_Counter = (ONE_TICK);
 	while (--ticks) {
-while (!(timer_IntStatus & TIMER1_RELOADED));	// Wait until the timer has reloaded
-timer_IntStatus = TIMER1_RELOADED;				// Acknowledge the reload
+		while (!(timer_IntStatus & TIMER1_RELOADED));	// Wait until the timer has reloaded
+		timer_IntStatus = TIMER1_RELOADED;				// Acknowledge the reload
 	}
 }
 
 /* Draws kong for use in cinematics */
 void render_kong(void) {
 	uint24_t x = kong.x;
+
 	if (kong.sprite) {
 		if (kong.sprite == 1)	x -= 4;
 		else if (kong.sprite == 3 || kong.sprite == 7 || kong.sprite == 9)	x++;
@@ -187,6 +195,7 @@ void draw_heart(gfx_image_t* sprite, uint8_t x, uint8_t y) {
 	gfx_BlitRectangle(gfx_buffer, x, y, 16, 13);	// Maybe use bool isbroken instead?
 }
 
+/* play end cinematic for all four stages */
 void end_stage_cinematic(void) {
 	gfx_Blit(gfx_screen);
 	gfx_Sprite_NoClip((gfx_image_t*)kong.background_data, kong.x_old, kong.y_old);
@@ -199,13 +208,17 @@ void end_stage_cinematic(void) {
 		kong.sprite = 0;
 		render_kong();
 
-		gfx_FillRectangle_NoClip(104, 72, 112, 160);
+		waitTicks(0x20);
+
+		// Clear space
+		gfx_FillRectangle_NoClip(112, 72, 96, 160);
 		for (y = 200; y <= 224; y += 8)
 			for (x = 112; x <= 200; x += 8)
 				gfx_Sprite_NoClip(girder_circle, x, y);
+		gfx_TransparentSprite_NoClip(jumpman_sprite[jumpman.dir][jumpman.sprite], jumpman.x - 7, jumpman.y - 15);
+		gfx_BlitRectangle(gfx_buffer, 112, 72, 96, 160);
 
-		gfx_BlitRectangle(gfx_buffer, 104, 72, 112, 160);
-
+		// Kong beating chest
 		kong.sprite = 4;
 		for (i = 0; i < 15; i++) {
 			waitTicks(8);
@@ -224,14 +237,18 @@ void end_stage_cinematic(void) {
 
 		while (kong.y < 200) {
 			render_kong();
-			waitTicks(1);
-			if (kong.y == 198) {
-				gfx_FillRectangle_NoClip(104, 32, 112, 40);
+			
+			// If kong is one pixel above the ground, move girder underneath pauline down
+			if (kong.y == 199) {	// maybe 198?
+				gfx_FillRectangle_NoClip(104, 32, 112, 8);
+				gfx_FillRectangle_NoClip(119, 40, 82, 32);
 				for (x = 104; x <= 208; x += 8)
 					gfx_Sprite_NoClip(girder_circle, x, 72);
 				gfx_BlitRectangle(gfx_buffer, 104, 32, 112, 48);
 			}
+
 			kong.y++;
+			waitTicks(1);
 		}
 
 		// move pauline down
@@ -243,7 +260,6 @@ void end_stage_cinematic(void) {
 
 		// Roll kongs eyes
 		kong.sprite = 11;
-
 		for (frameCounter = 0; frameCounter < 32; frameCounter++) {
 			render_kong();
 			
@@ -260,19 +276,19 @@ void end_stage_cinematic(void) {
 			waitTicks(8);
 		}
 
-		waitTicks(128);
+		waitTicks(224);
 	}
 	else {										// Stage Barrels, Elevators or Conveyors
 		// Step 1 of 6: update kong and draw heart(1 of 5 for conveyors)
 		draw_pauline(false);
-		draw_heart(heart, 154, 16);
+		draw_heart(heart, 154, 10);
 		kong.sprite = 0;
 		render_kong();
 
 		if (game.stage == STAGE_CONVEYORS) {	// Stage Conveyors
-			// Step 2 of 5:
+			// Step 2 of 5: reverse conveyor if needed and move kong to ladder
 			conveyor[Top].reverseCounter = 0;
-			if (kong.x > 104 && conveyor[Top].direction < 127 || kong.x < 104 && conveyor[Top].direction > 127)
+			if (kong.x > 104 && conveyor[Top].direction < 127 || kong.x < 104 && conveyor[Top].direction < 127)
 				conveyor[Top].reverseCounter = 1;
 
 			while (kong.x != 104) {
@@ -299,7 +315,7 @@ void end_stage_cinematic(void) {
 		}
 		// step 5 of 6: climb ladder with pauline(same as step 4 from conveyors)
 		kong.sprite = 8;
-		draw_heart(heart_broken, 153, 16);
+		draw_heart(heart_broken, 153, 10);
 		gfx_FillRectangle_NoClip(pauline.x, pauline.y, 16, 22);
 		gfx_BlitRectangle(gfx_buffer, pauline.x, pauline.y, 16, 22);
 
@@ -307,6 +323,6 @@ void end_stage_cinematic(void) {
 			kong_climb_ladder();
 		}
 		// step 6 of 6: determine next level and add bonus to score(same as step 5 from conveyors)
-		waitTicks(0x40);
+		waitTicks(0x30);
 	}
 }
